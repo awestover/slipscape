effb74461329b6e63cae2077a9255aaba4cbb78d
Alek Westover <alek.westover@gmail.com>
2019-12-23 22:18:11 -0500

    some more cool stuff (minor aesthetic edits)

--- practice/lightSketch.js
+++ practice/lightSketch.js
@@ -36,25 +36,40 @@ var scene = new THREE.Scene();
 let light = new THREE.AmbientLight(0x000000);
 scene.add(light);
 
-const spacing = 4;
-const decay = 0.7;
+const spacing = 8;
+const decay = 0.8;
 const numBoxes = 6;
 const dim = spacing*numBoxes;
 const center = new THREE.Vector3(dim/2,dim/2,dim/2);
 
+let cubes = [];
+
 for (let x = 0; x < numBoxes; x++){
   for (let y = 0; y < numBoxes; y++){
     for (let z = 0; z < numBoxes; z++){
-      const geometry = new THREE.BoxGeometry(1,1,1);
-      const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // green-blue
+      let geometry;
+      if (Math.random() < 0.5){
+        geometry = new THREE.BoxGeometry(1,1,1);
+      }
+      else{
+        geometry = new THREE.TorusGeometry( 1, 0.3, 8, 32 );
+      }
+      let material;
+      if(Math.random() < 0.5){
+        material = new THREE.MeshPhongMaterial({color: "#44aa88"});
+      }
+      else{
+        material = new THREE.MeshPhongMaterial({color: "#ffffff"});
+      }
       const cube = new THREE.Mesh(geometry, material);
       cube.rotation.x = Math.random()*Math.PI*2;
       cube.rotation.y = Math.random()*Math.PI*2;
       cube.rotation.z = Math.random()*Math.PI*2;
-      cube.position.x = (x+(Math.random()-0.5)*decay)*spacing;
-      cube.position.y = (y+(Math.random()-0.5)*decay)*spacing;
-      cube.position.z = (z+(Math.random()-0.5)*decay)*spacing;
+      cube.position.x = (x+(Math.random()-0.5)*2*decay)*spacing;
+      cube.position.y = (y+(Math.random()-0.5)*2*decay)*spacing;
+      cube.position.z = (z+(Math.random()-0.5)*2*decay)*spacing;
       scene.add(cube);
+      cubes.push(cube);
     }
   }
 }
@@ -62,7 +77,7 @@ for (let x = 0; x < numBoxes; x++){
 let ptLights = [];
 let ptLightBlobs = [];
 let ptLightVels = [];
-let ptLightColors = ["#ffff00", "#0000ff", "#00ff00", "#ff0000"];
+let ptLightColors = ["#ffff00", "#0000ff", "#00ff00", "#ff0000", "#00FFFF", "#ffffff"];
 
 for(let i in ptLightColors){
   ptLights.push( new THREE.PointLight( ptLightColors[i], 1, 100, 2) );
@@ -94,8 +109,6 @@ var camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 ); windo
 // this isn't cool and smooth YET ;0
 function alekRandomWalk(i){
   if (Math.random() < 0.003){
-    console.log("its the wind");
-    // push in a random dirrection
     ptLightVels[i].add(new THREE.Vector3(Math.random()-0.5, Math.random()-0.5, Math.random()-0.5));
     ptLightVels[i].normalize();
     ptLightVels[i].multiplyScalar(lightSpeed);
@@ -106,14 +119,10 @@ function alekRandomWalk(i){
     ptLights[i].position.y < 0 || ptLights[i].position.y > dim ||
     ptLights[i].position.z < 0 || ptLights[i].position.z > dim){
 
-    console.log(i + " getting bumped");
-
-    // if() {// vel not "pointing at center"
-      ptLightVels[i].subVectors(center, ptLights[i].position);
-      ptLightVels[i].normalize();
-      ptLightVels[i].multiplyScalar(lightSpeed);
-      ptLights[i].position.add(ptLightVels[i]);
-    // }
+    ptLightVels[i].subVectors(center, ptLights[i].position);
+    ptLightVels[i].normalize();
+    ptLightVels[i].multiplyScalar(lightSpeed);
+    ptLights[i].position.add(ptLightVels[i]);
   }
 
   ptLightBlobs[i].position.copy(ptLights[i].position);
@@ -130,6 +139,12 @@ function animate() {
 
   for(let i in ptLights){
     alekRandomWalk(i);
+  }
+
+  for(let i in cubes){
+    cubes[i].rotation.x += 0.05/13;
+    cubes[i].rotation.y += 0.05/17;
+    cubes[i].rotation.z += 0.05/19;
   }
 
 	renderer.render( scene, camera );
